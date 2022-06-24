f7e899be8026fe566e70ba292f937f235cf5bf94
KarryZ <katejidkova@gmail.com>
2022-06-24 14:26:51 +0300

    implement putMouseUp

--- src/controller/comandsController.js
+++ src/controller/comandsController.js
@@ -1,14 +1,14 @@
 import robot from 'robotjs';
 
-let command,  commandX, commandY;
+let command,  firstParam, secondParam;
 
 
 
-export const commandWithParams = (data) => {
-   
-    [ command,  commandX, commandY  ] = data.split(" ");
-
-   return `${command},${commandX},${commandY}`; 
+export const commandWithParams = (data) => {   
+    [ command,  firstParam, secondParam ] = data.split(" ");   
+    firstParam = +firstParam;
+    secondParam = +secondParam;
+    return secondParam ? `${command} ${firstParam},${commandY}` : `${command} ${firstParam}`; 
 }
 
 export const switchCommands =  () => {
@@ -16,13 +16,24 @@ export const switchCommands =  () => {
         case "mouse_position":
             return getCurrentPositions();
         break;
+        case "mouse_up":
+            return putMouseUp();
+        break;
     
         default:
             break;
     }
 }
 
-export const getCurrentPositions = () => {  
+export const getCurrentPositions = (bWithText) => {  
     const { x, y }  = robot.getMousePos();
-    return `mouse_position ${x},${y})`;
+    return `mouse_position ${x},${y}`;
+}
+
+export const putMouseUp = () => {
+    const { x, y }  = robot.getMousePos(); 
+    console.log("current pos", x, y);   
+    robot.moveMouse(x, y - firstParam);
+    console.log("new pos", x, y - firstParam);
+    return `mouse_up ${firstParam}`;
 }
